1b9f389aebe5cf4231d67faabe2d5b51748b3fd9
kau12757 <kau12757@sheridancollege.ca>
2023-03-08 12:56:02 -0500

    commit1

---
+++ src/mrev1/MRev1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mrev1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author kamal
+ */
+public class MRev1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        System.out.println(word);
+        // TODO code application logic here
+    }
+    
+}
